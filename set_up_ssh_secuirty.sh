634c4c4b521bbe6b7addeea5b2a05b290d639f72
kkarwowski <66829074+kkarwowski@users.noreply.github.com>
2021-04-18 19:12:22 +0100

    Create set_up_ssh_secuirty.sh

---
+++ set_up_ssh_secuirty.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Creates copy of original /etc/ssh/sshd_config file and then appends to original following lines
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.copy
+echo "Created copy of original file as /etc/ssh/sshd_config.copy"
+echo "PasswordAuthentication no
+UsePAM no
+PermitRootLogin no
+PermitRootLogin prohibit-password" >> /etc/ssh/sshd_config
+echo "PermitRootLogin no"
+echo "PermitRootLogin prohibit-password"
+echo "Changes have been made to /etc/ssh/sshd_config"
+
+

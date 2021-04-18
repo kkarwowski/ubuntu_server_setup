2c22a46ea462f837df16ce52dfa609e3720bbc48
kkarwowski <66829074+kkarwowski@users.noreply.github.com>
2021-04-18 18:57:44 +0100

    Update set_date_uk_GB.sh

--- set_date_uk_GB.sh
+++ set_date_uk_GB.sh
@@ -2,3 +2,4 @@
 # Unlinking current time zone and setting up London time zone
 sudo unlink /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Europe/London /etc/localtime
+echo "Timezone has been set up as London."
